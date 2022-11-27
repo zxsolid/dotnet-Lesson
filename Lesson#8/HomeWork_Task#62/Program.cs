75732afc4fd2b2050099f3698e145735c3e7bd53
zxsolid <stasbaydak@gmail.com>
2022-11-27 23:18:23 +0300

    HomeWork#8/9

---
+++ Lesson#8/HomeWork_Task#62/Program.cs
@@ -0,0 +1,35 @@
+﻿// Напишите программу, которая заполнит спирально массив 4 на 4.
+int[,] FillArray(int row, int col)
+{
+    int[,] array = new int[row, col];
+    int num = 1;
+    int i = 0;
+    int j = 0;
+
+    while (num <= row * col)
+    {
+        array[i, j] = num;
+        if (i <= j + 1 && i + j < row - 1) j++;
+        else if (i < j && i + j >= row - 1) i++;
+        else if (i >= j && i + j > row - 1) j--;
+        else i--;
+
+        num++;
+    }
+    return array;
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{String.Join(',', array[i, j])} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+int[,] array = FillArray(4, 4);
+PrintArray(array);
---
+++ Lesson#9/HomeWork_Task#64/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1. 
+// Выполнить с помощью рекурсии.
+Console.Clear();
+
+string PrintNums(int n = 8)
+{
+    if (n == 1) return n.ToString();
+    return $"{n}, {PrintNums(n - 1)} ";
+}
+
+
+
+Console.WriteLine("Введите N");
+int n = int.Parse(Console.ReadLine()!);
+
+Console.Write(PrintNums(n));
